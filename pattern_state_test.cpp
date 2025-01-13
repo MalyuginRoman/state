aea001ab122175c4125608be3ffddf5da87ead14
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-13 14:22:53 +0500

    Create pattern_state_test.cpp

---
+++ pattern_state_test.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "math.h"
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <iostream>
+#include <thread>
+#include "ioc.h"
+#include "icommand.h"
+#include "object.h"
+#include "safequeue.h"
+#include "exceptionhandler.h"
+#include "eventloop.h"
+#include "imessage.h"
+#include <thread>
+
+class pattern_state_test : public CPPUNIT_NS::TestCase
+{
+CPPUNIT_TEST_SUITE(pattern_state_test);
+  CPPUNIT_TEST(test1);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+void setUp(void) {}
+void tearDown(void){}
+
+protected:
+  void test1(void)
+{
+    Test();
+}
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(pattern_state_test);
+
+int main()
+{
+CPPUNIT_NS::TestResult controller;
+
+CPPUNIT_NS::TestResultCollector result;
+controller.addListener(&result);
+
+CPPUNIT_NS::BriefTestProgressListener progress;
+controller.addListener(&progress);
+
+CPPUNIT_NS::TestRunner runner;
+runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+runner.run(controller);
+
+return result.wasSuccessful() ? 0 : 1;
+}
