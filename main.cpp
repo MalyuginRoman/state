246f02050a12596bbab623445a555456e4923c90
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-21 08:51:55 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,4 @@
 #include "Pattern_state.h"
-#include <netinet/in.h> //structure for storing address information 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <sys/socket.h> //for socket APIs 
-#include <sys/types.h> 
 
 int main(int, char **)
 {
